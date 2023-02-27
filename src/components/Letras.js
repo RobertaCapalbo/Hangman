e2323c8a35735cabade1b86c6e5c8a00f92dc112
Roberta Capalbo <robertacapalbo@hotmail.com>
2023-02-27 11:14:52 -0300

    ajustes finos

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -18,7 +18,7 @@ export default function Letras({activeLetters, inactiveLetters, addGuessedLetter
         {alfabeto.map((letter)=> {const isActive = !activeLetters.includes(letter)
         const isInactive = !inactiveLetters.includes(letter)
        return(
-        <button class="a" onClick={() => addGuessedLetters(letter)} isActive={isActive && isInactive} data-test="letter" style={{backgroundColor: guessedLetters.includes(letter) ? '#9FAAB5' : '#E1ECF4'}}>{letter}</button>
+        <button class="a" disabled={guessedLetters.includes(letter)} onClick={() => addGuessedLetters(letter)} isActive={isActive && isInactive} data-test="letter" style={{backgroundColor: guessedLetters.includes(letter) ? '#9FAAB5' : '#E1ECF4'}}>{letter}</button>
        )
       }
         )}
